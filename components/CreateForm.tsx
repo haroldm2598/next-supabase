87828d5e5be711c6cd063f9459215edc7b986d15
Harold <cvuyante24@gmail.com>
2024-08-16 21:04:20 +0800

    commit

--- components/CreateForm.tsx
+++ components/CreateForm.tsx
@@ -15,75 +15,66 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 
+type FormFieldNames = 'title' | 'description' | 'rating';
+
 const formSchema = z.object({
 	title: z.string().min(2, {
 		message: 'title must be at least 2 characters.'
 	}),
-	desc: z.string().min(2, {
+	description: z.string().min(2, {
 		message: 'description is required'
 	}),
-	rating: z.string().min(1, {
+	rating: z.number().min(1, {
 		message: 'rating is required'
 	})
 });
+
+const formArr: { name: FormFieldNames; placeholder: string; type: string }[] = [
+	{ name: 'title', placeholder: 'enter a title', type: 'text' },
+	{ name: 'description', placeholder: 'enter a description', type: 'text' },
+	{ name: 'rating', placeholder: 'enter a rating', type: 'number' }
+];
 
 export default function CreateForm() {
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			title: '',
-			desc: '',
-			rating: ''
+			description: '',
+			rating: 0
 		}
 	});
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
+	async function onSubmit(values: z.infer<typeof formSchema>) {
 		console.log(values);
 	}
+
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className=' space-y-8'>
-				<FormField
-					control={form.control}
-					name='title'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Title:</FormLabel>
-							<FormControl>
-								<Input placeholder='shadcn' className='w-full' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-
-				<FormField
-					control={form.control}
-					name='desc'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Description:</FormLabel>
-							<FormControl>
-								<Input placeholder='shadcn' className='w-full' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-
-				<FormField
-					control={form.control}
-					name='rating'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Rating:</FormLabel>
-							<FormControl>
-								<Input placeholder='shadcn' className='w-full' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+				{formArr.map((item, index) => (
+					<FormField
+						key={index}
+						control={form.control}
+						name={item.name}
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>
+									{item.name.charAt(0).toUpperCase() + item.name.slice(1)}:
+								</FormLabel>
+								<FormControl>
+									<Input
+										placeholder={item.placeholder}
+										className='w-full'
+										{...field}
+										type={item.type}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				))}
 				<Button className='bg-customGreen' type='submit'>
 					Create Smoothie Recipe
 				</Button>
